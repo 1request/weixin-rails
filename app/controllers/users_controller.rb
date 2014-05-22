a37ff2377df0678d296559149c69c45e872177e8
kaeli <puiesabu@gmail.com>
2014-05-22 15:49:02 +0800

    Rename id to weixin_id

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -64,11 +64,11 @@ class UsersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-      @user = User.find(params[:weixin_id])
+      @user = User.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:weixin_id)
+      params.require(:user).permit(:id, :weixin_id)
     end
 end
