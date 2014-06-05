956e3360d5662b566acdaa78001f20f3c64ca2a2
kaeli <puiesabu@gmail.com>
2014-06-05 15:57:48 +0800

    Create set_client private method

--- app/controllers/kf_controller.rb
+++ app/controllers/kf_controller.rb
@@ -1,15 +1,14 @@
 class KfController < ApplicationController
   skip_before_filter :verify_authenticity_token 
   after_filter :set_access_control_headers
+  before_action :set_client
 
   def index
-    client ||= WeixinAuthorize::Client.new("wxe2e163d3337f28ee", "0ce603e4068fd1f8ee5ef324473d5687")
-    client.followers.result['data']['openid'].each do |follower|
+    @client.followers.result['data']['openid'].each do |follower|
       customer = Customer.where(:fromUser => follower).first
       customer = Customer.create(:fromUser => follower) unless customer
 
-      client ||= WeixinAuthorize::Client.new("wxe2e163d3337f28ee", "0ce603e4068fd1f8ee5ef324473d5687")
-      customer.user_info = client.user(follower).result
+      customer.user_info = @client.user(follower).result
       customer.save
     end
 
@@ -17,16 +16,18 @@ class KfController < ApplicationController
   end
 
   def create
-    client ||= WeixinAuthorize::Client.new("wxe2e163d3337f28ee", "0ce603e4068fd1f8ee5ef324473d5687")
-    client.send_text_custom(params[:weixin_id], params[:q])
+    @client.send_text_custom(params[:weixin_id], params[:q])
 
     head :created
   end
 
+private
   def set_access_control_headers 
     headers['Access-Control-Allow-Origin'] = 'http://localhost:4000' 
     headers['Access-Control-Request-Method'] = '*' 
   end
 
-
+  def set_client
+    @client ||= WeixinAuthorize::Client.new("wxe2e163d3337f28ee", "0ce603e4068fd1f8ee5ef324473d5687")
+  end
 end
