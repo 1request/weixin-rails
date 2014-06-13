f76cd6b3621470fd2b6bad23ae37fcc00704f2dd
kaeli <puiesabu@gmail.com>
2014-06-13 14:11:36 +0800

    Set pem file for ssh authentication for Capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,11 @@ require 'cape'
 set :stages, %w(staging production)
 set :default_stage, "production"
  
- 
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+ssh_options[:auth_methods] = ["publickey"]
+ssh_options[:keys] = ["/Users/harryng/Downloads/kaeli.pem"]
+
 Cape do
   mirror_rake_tasks :dev
 end
