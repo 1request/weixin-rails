6d8881bf7a721bb4ad460009c71da1a9fe8344e3
kaeli <puiesabu@gmail.com>
2014-06-13 17:12:39 +0800

    Use apis#index instead of apis#show

--- app/controllers/apis_controller.rb
+++ app/controllers/apis_controller.rb
@@ -1,5 +1,5 @@
 class ApisController < ApplicationController
-  def show
+  def index
     @weixin_secret_key = WeixinRailsMiddleware.config.weixin_secret_string
     @weixin_token = WeixinRailsMiddleware.config.weixin_token_string
   end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Wechat::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root 'apis#show'
+  root 'apis#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
