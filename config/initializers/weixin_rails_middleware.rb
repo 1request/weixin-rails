37af15d90ef26fcbc4f1e9a405b5d494564b0226
Kaeli Lo <puiesabu@gmail.com>
2014-06-25 11:21:16 +0800

    Use Account weixin id & secret key instead of config

--- config/initializers/weixin_rails_middleware.rb
+++ config/initializers/weixin_rails_middleware.rb
@@ -12,8 +12,8 @@ WeixinRailsMiddleware.configure do |config|
   ## Here configure is for you DON'T WANT TO SAVE your public account into database ##
   # Or the other configure is fit for only one weixin public_account
   # If you config `weixin_token_string`, so it will directly use it
-  config.weixin_token_string = '6b5c473f52cd39242238ee64'
+  #config.weixin_token_string = '6b5c473f52cd39242238ee64'
   # using to weixin server url to validate the token can be trusted.
-  config.weixin_secret_string = '0WXiiyHp3L0EhVJMP95ckpCyr8jTmYC8'
+  #config.weixin_secret_string = '0WXiiyHp3L0EhVJMP95ckpCyr8jTmYC8'
 
 end
