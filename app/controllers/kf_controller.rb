a9563374c55ad95d907463588cf7fc349ec7d1e3
Kaeli Lo <puiesabu@gmail.com>
2014-06-26 13:25:59 +0800

    Use weixin account id instead of config for getting customer list and sending text to customer

--- app/controllers/kf_controller.rb
+++ app/controllers/kf_controller.rb
@@ -1,17 +1,16 @@
 class KfController < ApplicationController
   skip_before_filter :verify_authenticity_token 
   after_filter :set_access_control_headers
-  before_action :set_client
 
   def index
-    @client.followers.result['data']['openid'].each do |follower|
+    client.followers.result['data']['openid'].each do |follower|
       customer = Customer.where(:fromUser => follower).first
       unless customer
         id = BSON::ObjectId.new
         customer = Customer.create(_id: id.to_s, fromUser: follower)
       end
 
-      customer.user_info = @client.user(follower).result
+      customer.user_info = client.user(follower).result
       customer.save
     end
 
@@ -19,18 +18,24 @@ class KfController < ApplicationController
   end
 
   def create
-    @client.send_text_custom(params[:weixin_id], params[:q])
+    client.send_text_custom(params[:weixin_id], params[:q])
 
     head :created
   end
 
-private
-  def set_access_control_headers 
-    headers['Access-Control-Allow-Origin'] = 'http://localhost:4000' 
-    headers['Access-Control-Request-Method'] = '*' 
-  end
+  private
+    def set_access_control_headers 
+      headers['Access-Control-Allow-Origin'] = 'http://localhost:4000' 
+      headers['Access-Control-Request-Method'] = '*' 
+    end
 
-  def set_client
-    @client ||= WeixinAuthorize::Client.new("wxe2e163d3337f28ee", "0ce603e4068fd1f8ee5ef324473d5687")
-  end
+    def client
+      @clients ||= {}
+      @clients[params[:gh_id]] ||= connect_client
+    end
+
+    def connect_client
+      account = Account.where(gh_id: params[:gh_id]).first
+      WeixinAuthorize::Client.new(account.app_id, account.app_secret)
+    end
 end
