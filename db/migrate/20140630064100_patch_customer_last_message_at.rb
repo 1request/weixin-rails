403d1d4a744ea432213d689d1d25164118817527
Kaeli Lo <puiesabu@gmail.com>
2014-06-30 16:11:55 +0800

    Fix last message at script problem

--- db/migrate/20140630064100_patch_customer_last_message_at.rb
+++ db/migrate/20140630064100_patch_customer_last_message_at.rb
@@ -2,6 +2,6 @@
 
 Customer.all.each do |customer|
   last_message = Message.where(customer_id: customer._id).order(created_at: :desc).first
-  customer.last_message_at = last_message.created_at
+  customer.last_message_at = last_message.created_at if last_message
   customer.save!
 end
